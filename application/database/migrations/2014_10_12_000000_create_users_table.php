696e92d6968df142874487b6bdbf5a18378c9694
Mizanur Rahman <mizanur.rahman@smartwebsource.com>
2017-12-30 13:56:49 +0600

    user migration setup

--- application/database/migrations/2014_10_12_000000_create_users_table.php
+++ application/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email',50)->unique();
+            $table->string('phone',20)->unique();
+            $table->string('username',16)->unique();
             $table->string('password');
+            $table->string('role',20);
+            $table->boolean('active')->default(false);
+            $table->timestamp('last_login')->nullable();
+            $table->ipAddress('last_login_ip',50)->nullable();
+            $table->string('photo',250)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
